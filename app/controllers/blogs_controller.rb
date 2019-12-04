68597e8b5dd665ed18d64beb79ab7c339e6ff7aa
tatsuya oozeki <tasuya6281@icloud.com>
2019-12-04 19:04:26 +0900

    Add: blog new/create/show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,33 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show]
   def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find([params[:id]])
   end
 end
