a7ddd5b24a8f50a599754a6c1e5c2667ca83e9cb
tatsuya oozeki <tasuya6281@icloud.com>
2019-12-09 16:37:35 +0900

    Add: model devise user, devise view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'blogs#index'
   resources :blogs do
     collection do
       post :confirm
     end
+  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
