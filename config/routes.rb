e7145dac95a231c4fb91f37c953e7c4209fbe0ae
tatsuya oozeki <tasuya6281@icloud.com>
2019-12-13 17:11:39 +0900

    Add: ユーザー詳細ボタン

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
   resources :relationships, only: [:create, :destroy]
 
   if Rails.env.development?
