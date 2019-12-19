cf9d9f096040a6cc15a2ffd40d1c07547dc96af4
tatsuya oozeki <tasuya6281@icloud.com>
2019-12-19 13:27:19 +0900

    Add: message model, message table

---
+++ db/migrate/20191219042123_create_messages.rb
@@ -0,0 +1,14 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.integer :conversation_id
+      t.integer :user_id
+      t.references :conversation, foreign_key: true
+      t.references :user, foreign_key: true
+      t.boolean :read, default: false
+
+      t.timestamps
+    end
+  end
+end
